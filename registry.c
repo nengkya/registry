44047285c32e4e7182f8954280471ce32faae96b
Hasna Dzakiyya Makin <nengkya@yahoo.com>
2024-12-21 18:28:21 +0700

    #include <windows.h>

---
+++ registry.c
@@ -0,0 +1,16 @@
+#include <windows.h>
+
+
+int main() {
+
+    /*Handle to Registry Key
+
+      typedef HANDLE HKEY;
+
+      typedef PVOID HANDLE;
+
+      typedef void * PVOID;
+    */
+    HKEY hRootKey;
+
+}
